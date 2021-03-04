8296f6f72053d6db23b628475ac27b26ad19de1f
Paweł <rosiu978@gmail.com>
2021-03-04 22:13:59 +0100

    init

---
+++ Array challenge/challegnepart2.js
@@ -0,0 +1,25 @@
+
+const bill = 100;
+
+const calcTip = function (bill) {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15;
+    } else {
+        return bill * 0.2;
+    }
+}
+
+console.log(calcTip(bill));
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(bills, tips)
+
+function billsTips(num) {
+
+    const every = bills[num] + tips[num];
+    return every;
+}
+const total = `${billsTips(0)}, ${billsTips(1)}, ${billsTips(2)}`          //bills[0] + tips[0];
+console.log(total);
