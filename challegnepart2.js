39e5854c078a6373ef2acbea166e6307698a332e
Paweł <rosiu978@gmail.com>
2021-03-04 22:09:29 +0100

    Completing challenge #2

--- challegnepart2.js
+++ challegnepart2.js
@@ -1,6 +1,7 @@
+
 const bill = 100;
 
-const tip = function (bill) {
+const calcTip = function (bill) {
     if (bill >= 50 && bill <= 300) {
         return bill * 0.15;
     } else {
@@ -8,5 +9,17 @@ const tip = function (bill) {
     }
 }
 
+console.log(calcTip(bill));
 
-console.log(tip(bill));
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(bills, tips)
+
+function billsTips(num) {
+
+    const every = bills[num] + tips[num];
+    return every;
+}
+const total = `${billsTips(0)}, ${billsTips(1)}, ${billsTips(2)}`          //bills[0] + tips[0];
+console.log(total);
