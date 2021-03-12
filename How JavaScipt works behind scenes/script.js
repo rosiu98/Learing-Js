e3311df08557bd049dcbee9ecff4ed1c3ac18aa9
Paweł <73038209+rosiu98@users.noreply.github.com>
2021-03-12 15:22:26 +0100

    Add files via upload

---
+++ How JavaScipt works behind scenes/script.js
@@ -0,0 +1,191 @@
+'use strict';
+
+// function calcAge(birthYear){
+// 	const age = 2037 - birthYear;
+	
+// 	function printAge(){
+// 		let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+// 		console.log(output);
+
+// 		if(birthYear >= 1981 && birthYear <= 1996){
+// 			var millenial = true;
+// 			// Creating New veriable with same name as outer scope's veriable
+// 			const firstName = 'Steven';
+
+// 			// Reassigning outer scope's variable
+// 			output = 'NEW OUTPUT!'
+
+// 			const str = `Oh, and you're a millenial, ${firstName}`;
+// 			console.log(str);
+		
+
+// 		function add(a,b){
+// 			return a + b
+// 		}
+		
+// 	}
+// 		// console.log(str);
+// 		console.log(millenial);
+// 		// console.log(add(2,3));
+// 		console.log(output);
+// 	}
+
+// 	printAge();
+// 	return age;
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1991);
+// // console.log(age);
+// // printAge();
+
+// // Variables
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
+
+
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
+
+// //Functions
+// console.log(addDecl(2,3));
+// // console.log(addExpr(2,3));
+// console.log(addArrow);
+// // console.log(addArrow(2,3));
+
+
+
+// function addDecl(a,b){
+// 	return a + b
+// }
+
+// const addExpr = function(a,b){
+// 	return a + b
+// }
+
+// var addArrow = (a,b) => a + b;
+
+// // Example
+// console.log(numProducts);
+// if(!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+
+// function deleteShoppingCart(){
+// 	console.log('All products deleted!');
+// }
+
+
+// var x = 1
+// let y = 2;
+// const z =3;
+
+// console.log( x === window.x);
+// console.log( y === window.x);
+// console.log( z === window.x);
+
+
+// console.log(this);
+
+// const calcAge = function(birthYear){
+// 	console.log(2037 - birthYear);
+// 	console.log(this);
+// }
+// calcAge(1991);
+
+// const calcAgeArrow =(birthYear) =>{
+// 	console.log(2037 - birthYear);
+// 	// console.log(this);
+// }
+// calcAgeArrow(1988);
+
+// const jonas = {
+// 	year: 2037,
+// 	calcAge: function(birthYear){
+// 		console.log(this);
+// 		console.log(this.year -birthYear)
+// 	}
+// }
+// jonas.calcAge(1999);
+
+// const matilda = {
+// 	year: 2017,
+// };
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge(1999);
+
+// const f = jonas.calcAge;
+// console.log(f);
+// f(1950);
+
+// var firstName = 'matilda';
+
+//  const jonas = {
+//  	firstName: 'Jonas',
+//  	year: 2037,
+//  	calcAge: function(birthYear){
+//  		// console.log(this);
+//  		console.log(this.year -birthYear)
+
+//  		// Solution 1
+//  		// const self = this;
+//  		// const isMillenial = function(){
+//  		// 	console.log(self);
+//  		// 	console.log(self.year >= 1981 && self.year <= 1996 );
+//  		// }
+
+ 		
+//  		const isMillenial = () =>{
+//  			console.log(this);
+//  			console.log(this.year >= 1981 && this.year <= 1996 );
+//  		}
+
+//  		isMillenial();
+// 	},
+
+// 	greet:() =>{
+// 		console.log(this),
+// 		console.log(`Hey ${this.firstName}`)
+// 	}
+// }
+
+// jonas.greet();
+// jonas.calcAge(1990)
+
+// // Arguments keyword
+// const addExpr = function (a,b) {
+// 	console.log(arguments)
+// 	return a + b;
+// }
+// addExpr(2, 5);
+// addExpr(2, 5, 8, 12)
+
+
+// var addArrow = (a, b) =>{
+// 	console.log(arguments);
+//  return a + b;
+
+// }
+// addArrow(2, 5, 8);
+
+
+let age  = 30;
+let oldAge = age;
+
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+	name: 'Jonas',
+	age: 30,
+};
+
+const friend = me;
+friend.age = 27;
+
+console.log('Friend', friend);
+console.log('Me', me);
