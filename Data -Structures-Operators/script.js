a3d5822d46aabad31d137d4e005773725d10a758
Paweł <rosiu978@gmail.com>
2021-03-15 13:30:31 +0100

    entries

--- Data -Structures-Operators/script.js
+++ Data -Structures-Operators/script.js
@@ -34,5 +34,7 @@ const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 for (const item of menu) console.log(item);
 
 for (const item of menu.entries()) {
-    console.log(item);
+    console.log(`${item[0] + 1}: ${item[1]}`);
 }
+
+// console.log([...menu.entries()]);
